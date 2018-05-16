89153b4d4c21a4a02af8798ffa3c9e9a452b0a6c
Andrew <andrew.vinocur@gmail.com>
2018-05-16 13:00:41 -0700

    boiler plate for controlled input for text area

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
 export default class CommentBox extends Component {
+
+
+
+  handleChange(event) {
+
+  }
+
   render() {
     return (
       <div className='comment-box'>
-        <textarea />
+        <textarea onChange={this.HandleChange.bind(this)} />
         <button>Submit Content</button>
       </div>
     );
