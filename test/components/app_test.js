480a7dedda78c5c6610064e29472130ef3f3bbac
Andrew <andrew.vinocur@gmail.com>
2018-05-16 12:21:47 -0700

    clears example

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -4,15 +4,6 @@ import App from '../../src/components/app';
 //Use 'describe' to group together similar tests
 describe('App', () => {
 
-  // Use 'it' to test a single attribute of a target
-  it('shows the correct text', () => {
 
-    //create an instance of app
-    const component = renderComponent(App);
-
-    // Use 'expect' to make an 'assertion' about a target
-    expect(component).to.contain('React simple starter');
-
-  });
 
 });
