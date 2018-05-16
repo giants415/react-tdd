7dcb0ed607d83e16dd7a65f186ed3d4badce38e4
Andrew <andrew.vinocur@gmail.com>
2018-05-16 12:24:29 -0700

    adds expect for comment box

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -3,7 +3,14 @@ import App from '../../src/components/app';
 
 //Use 'describe' to group together similar tests
 describe('App', () => {
+  let component;
 
+  beforeEach(() => {
+    component = renderComponent(App);
+  });
 
+  it('shows a comment box', () => {
+    expect(component.find('.comment-box')).to.exist;
+  });
 
 });
