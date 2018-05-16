9bc33ca577a02751a20f0f66b8c304f9a778afa2
Andrew <andrew.vinocur@gmail.com>
2018-05-16 13:47:45 -0700

    adds handle submit to reset state after submission

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -12,9 +12,15 @@ export default class CommentBox extends Component {
     this.setState({ comment: event.target.value });
   }
 
+  handleSubmit(event) {
+    event.preventDefault();
+
+    this.setState({ comment: '' });
+  }
+
   render() {
     return (
-      <form className='comment-box'>
+      <form onSubmit={this.handleSubmit.bind(this)} className='comment-box'>
         <textarea
           value={this.state.comment}
           onChange={this.handleChange.bind(this)} />
