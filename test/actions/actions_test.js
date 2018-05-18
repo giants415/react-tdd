e8a19a7fcca45d0376e12b14e7e364b5a9f93406
Andrew <andrew.vinocur@gmail.com>
2018-05-18 16:39:51 -0700

    sets up test specs

--- test/actions/actions_test.js
+++ test/actions/actions_test.js
@@ -1,13 +1,16 @@
 import { expect } from '../test_helper';
+import '../../src/actions';
 
 describe('actions', () => {
   describe('saveComment', () => {
     it('has the correct type', () => {
-
+      const action = saveComment();
+      expect(action.type).to.equal(SAVE_COMMENT);
     });
 
     it('has the correct payload', () => {
-
+      const action = saveComment('comment 1');
+      expect(action.payload).to.equal('comment 1');
     });
   });
 });
