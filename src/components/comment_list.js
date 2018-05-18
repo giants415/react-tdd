374a94344e5e6946417460d0c8d657e5e602fc41
Andrew <andrew.vinocur@gmail.com>
2018-05-18 13:40:09 -0700

    creates list fn

--- src/components/comment_list.js
+++ src/components/comment_list.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const CommentList = (props) => {
+  const list = props.comments.map(
+    comment => <li key={comment}>{comment}</li>
+  );
+
+
   return (
-    <ul className="comment-list"></ul>
+    <ul className="comment-list">{list}</ul>
   );
 }
 
