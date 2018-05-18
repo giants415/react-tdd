be219fb270cd6494d93ace021bed9081db3ad3fd
Andrew <andrew.vinocur@gmail.com>
2018-05-18 13:25:57 -0700

    boiler plate set up and initial specs to test for

--- test/components/comment_list_test.js
+++ test/components/comment_list_test.js
@@ -2,5 +2,17 @@ import { renderComponent, expect } from '../test_helper';
 import CommentList from '../../src/components/comment_list';
 
 describe('CommentList', () => {
-  
+  let component;
+
+  beforeEach(() => {
+    component = renderComponent(CommentList);
+  });
+
+  it('shows an li for each comment', () => {
+
+  });
+
+  it('shows each comment that is provided', () => {
+
+  });
 });
