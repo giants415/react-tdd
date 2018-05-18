35a61bc988e9d3e1228b047e8a99f96459e87f27
Andrew <andrew.vinocur@gmail.com>
2018-05-18 13:28:45 -0700

    passes in props and sets up first spec

--- test/components/comment_list_test.js
+++ test/components/comment_list_test.js
@@ -5,11 +5,13 @@ describe('CommentList', () => {
   let component;
 
   beforeEach(() => {
-    component = renderComponent(CommentList);
+    const props = { comments:['comment 1', 'comment 2'] };
+    component = renderComponent(CommentList, null, props);
   });
 
   it('shows an li for each comment', () => {
-
+    // find all comments on the list and i expect the list length to equal 2
+    expect(component.find('li').length).to.equal(2);
   });
 
   it('shows each comment that is provided', () => {
