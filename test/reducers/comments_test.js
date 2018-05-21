992817173e50e6473ef42f49b3581b6f4bf1a0d0
Andrew <andrew.vinocur@gmail.com>
2018-05-21 16:24:14 -0700

    passes in undefied type and empty object to fix test that now expect a type

--- test/reducers/comments_test.js
+++ test/reducers/comments_test.js
@@ -4,9 +4,9 @@ import { SAVE_COMMENT } from '../../src/actions/index';
 
 describe('Comments Reducer', () => {
   it('handles action with unknown type', () => {
-    expect(commentReducer()).to.be.instanceof(Array);
+    expect(commentReducer(undefined, {})).to.be.instanceof(Array);
     //alternative way to "deep check" that array result is empty
-    expect(commentReducer()).to.eql([]);
+    expect(commentReducer(undefined, {})).to.eql([]);
   });
 
   it ('handles action of type SAVE_COMMENT', () => {
