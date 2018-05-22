62f4baaf2e6f8516d53da3edf8393d612de02043
Andrew <andrew.vinocur@gmail.com>
2018-05-22 16:20:38 -0700

    4 steps to create a test helper file from scratch

---
+++ test/test_helper.js
@@ -0,0 +1,15 @@
+
+
+// set up testing env to run like a browser in the command line
+
+
+
+
+// build 'renderComponent' helper that should render a given react class
+
+
+
+// build helper for simulating events
+
+
+//set up chai-jquery
