aa95c8d11301831e1b5ef854b6647495ab00d8bc
Andrew <andrew.vinocur@gmail.com>
2018-05-22 16:29:26 -0700

    imports jsdom and sets up "fake" browser for testing in command line

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,8 +1,10 @@
-
+import jsom from 'jsdom';
 
 // set up testing env to run like a browser in the command line
 
-
+// initalizes and sets up "fake" browser
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.window = global.document.defaultView;
 
 
 // build 'renderComponent' helper that should render a given react class
