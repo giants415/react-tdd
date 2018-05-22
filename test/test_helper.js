fad742fe254a20aeb621ec72e24d89fff230b634
Andrew <andrew.vinocur@gmail.com>
2018-05-22 16:45:34 -0700

    imports jquery and passes in "Fake" dom that i created

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,10 +1,12 @@
 import jsom from 'jsdom';
+import jquery from 'jquery';
 
 // set up testing env to run like a browser in the command line
 
 // initalizes and sets up "fake" browser
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
+const $ = jquery(global.window);
 
 
 // build 'renderComponent' helper that should render a given react class
