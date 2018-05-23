c6fa15017c1eface310b4708928415184203968f
Andrew <andrew.vinocur@gmail.com>
2018-05-23 10:27:58 -0700

    passes in state and applies props to ComponentClass with spread operator to gain access to individual props rather than an object that lists all props

--- test/test_helper.js
+++ test/test_helper.js
@@ -19,8 +19,8 @@ const $ = jquery(global.window);
 // build 'renderComponent' helper that should render a given react class
 function renderComponent(ComponentClass, props, state) {
   const componentInstance = TestUtils.renderIntoDocument(
-    <Provider store={createStore(reducers)}>
-      <ComponentClass />
+    <Provider store={createStore(reducers, state)}>
+      <ComponentClass {...props} />
     </Provider>
   );
 
